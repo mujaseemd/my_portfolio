49d0b1f3232cbad56b3a724b1b160a70fe478cf8
mujaseemd <mujaseemd09@gmail.com>
2025-06-29 22:14:52 +0530

    Clean rebuild with correct AWS cert section

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my_portfolio/',
+  base: '/my_portfolio/', // must match GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'docs', // <--- This is what tells Vite to output to `docs/`
   },
 });
